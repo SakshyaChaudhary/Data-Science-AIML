f46bf086eae253bb14cae0355327ad4897be0379
SakshyaChaudhary <sakshyachaudhary2000@gmail.com>
2026-06-03 03:27:51 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,66 @@
+#Question 1-
+# (a) int-: Store integer which take 4 Byte
+num1=5
+print(type(num1))
+# (b) float-: Store decimal which take 4 Byte
+num2=10.5
+print(type(num2))
+# (c) String-: Sequence of characters
+Branch="CSE"
+print(type(Branch))
+# (d) Boolean-: Store Boolean value which is True/False
+ans=True
+print(type(ans))
+
+# Question 2-
+name="Sakshya"
+age=20
+city="Pilani"
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
+
+# Question 3-
+user=input("Enter your Name:")
+up_user=user.upper()
+print("Name in Upper Case:",up_user)
+print("Total character:",len(user))
+
+# Question 4-
+Sample="Hello world"
+print("Upper:",Sample.upper())
+print("Title:",Sample.title())
+print("Count of l:",Sample.count("l"))
+print("Index of w:",Sample.index("w"))
+Sample=Sample.replace("world","Guys")
+print("String after Replace:",Sample)
+
+# Question 5-
+Fruit=["Apple","Banana","Mango","Grapes","Pineapple"]
+print(Fruit)
+print("First:",Fruit[0],"& Last:",Fruit[-1])
+print("Length of List:",len(Fruit))
+
+# Question 6-
+lst=[10, 20, 30, 40, 50]
+lst.append(60)
+lst.remove(20)
+print(lst)
+
+# Question 7-
+# AI is the technology that enable Machine to think, learn and make decisions like humans.
+# Importance:-
+# ->Save Time and Effort
+# ->Automates repetitive tasks & Helps in better decision-making.
+# Uses:-
+# ->ChatGPT
+# ->Google Maps
+# ->Voice Assistants
+# ->Finance Management
+
+# Question 8-
+# (a) ChatGPT- AI as we already know
+# (b) Google Maps Route Prediction- It analyzes traffic and suggests the best route
+# (c) Calculator- not AI
+# (d) Netflix Recommendations- It suggests movies based on user preferences and viewing history.
+# (e) Voice Assistants (Alexa/Siri)- They understand voice commands and respond intelligently.
